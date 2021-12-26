3c31f35573b6f897be3f8190ae06f85c7424a748
Nin <ppeeranat.d@gmail.com>
2021-12-26 09:07:18 +0700

    Change picture to be mine picture in mobile

---
+++ src/components/Photo/MyPhoto.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-img
+        src="../../../assets/my-photo.jpg"
+        contain
+        :height="height"
+    ></v-img>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  props: {
+      height: {
+          type: String,
+          required: false,
+      }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,8 @@
       developer | photographer
     </div>
     <v-row style="height: 450px">
-      <v-img height="450px" :src="require('../../assets/home.png')"></v-img>
+      <MyPhoto v-if="isMobile" height="400px"/>
+      <v-img v-if="!isMobile" height="450px" :src="require('../../assets/home.png')"></v-img>
     </v-row>
     <div class="d-flex justify-center mt-12">
       Scroll down to know more about me<v-icon class="mx-3"
@@ -23,12 +24,21 @@
 </template>
 
 <script>
+import MyPhoto from "../components/Photo/MyPhoto.vue";
 export default {
-  name: "Home",
-  data() {
-    return {
-      screenWidth: screen.width,
-    };
-  },
+    name: "Home",
+    data() {
+        return {
+            screenWidth: screen.width,
+        };
+    },
+    computed: {
+        isMobile() {
+            return (this.$vuetify.breakpoint.name === "xs" ||
+                this.$vuetify.breakpoint.name === "sm" ||
+                this.$vuetify.breakpoint.name === "md");
+        },
+    },
+    components: { MyPhoto }
 };
 </script>
